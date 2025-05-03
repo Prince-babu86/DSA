aefd918e9a2f3b3464d5dc85b7290c196b53a790
Prince Babu <princebabu4495@gmail.com>
2025-05-03 12:36:50 +0530

    frist commit

---
+++ LEARNING/DSA BASIC/script.js
@@ -0,0 +1,80 @@
+// integer in javascript 
+
+
+
+
+// relations between integer and string 
+
+/*
+string + string = string (concatination)
+string + integer = string(concanitation)
+int + int = int (Arthimatic)
+
+
+*/
+
+
+// let a = 10
+// let b = 20
+
+// console.log("sum of " + a + " and" + b + " is" + a+b);
+
+// type costion 
+// console.log(1-"1");  // concat + addition 
+// console.log(1-"1");  // only subtract
+
+
+// let age = Number(prompt("Enter a age"));
+
+
+// console.log(age);
+
+
+// Swap Two varibales 
+
+
+let a = 10 
+let b = 20
+let temp ;
+
+
+// method 1
+// temp = a 
+// a = b 
+// b = temp
+
+
+// method 2
+
+// a += b // 30 
+// b = a - b 
+// a = a - b
+
+
+// method 3 
+
+[a ,b] = [b,a];
+
+//
+console.log(10%4);
+console.log(4%15);
+
+//
+console.log(Math.floor(10/3));
+console.log(10<=12);
+
+console.log(10==="10"); //  false
+console.log(5!==5); // false
+
+
+// increment and decrement 
+
+/*
+we cannot use the dicrectly increment and decrement on the number frist set a vraible and apply 
+
+*/
+
+console.log(++a);
+
+
+
